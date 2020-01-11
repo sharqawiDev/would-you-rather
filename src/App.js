0a51394f0279fddbc5a5cde1a64e2a76ba02dff7
Abdulrahman Elsharqawi <215110068@psu.edu.sa>
2020-01-11 21:13:03 +0300

    configuring and testing react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
+      <Button variant="dark">Hello</Button>
     </div>
   );
 }
