ba5b7fd1fd37b17fa87ba90cafcab89202251279
Abdulrahman Elsharqawi <215110068@psu.edu.sa>
2020-01-12 16:12:17 +0300

    dispatching the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Login from "./components/Login/Login"
+import { connect } from 'react-redux'
+import { handleInitialData } from './actions/shared'
 
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch((handleInitialData()))
+  }
+  render() {
+    return (
+      <div className="App" >
+        <Login />
+      </div>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
 }
 
-export default App;
+export default connect()(App)
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,4 +1,4 @@
-import { getInitialData } from '../utils/API'
+import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveQuestions } from './questions'
 import { setAuthedUser } from './authedUser'
