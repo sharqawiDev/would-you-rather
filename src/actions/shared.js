f89cd1c79f17a0e23c8fa897a9e614f9cdb4e5e7
Abdulrahman Elsharqawi <215110068@psu.edu.sa>
2020-01-12 14:16:35 +0300

    complete all Redux actions

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/API'
+import { receiveUsers } from './users'
+import { receiveQuestions } from './questions'
+import { setAuthedUser } from './authedUser'
+
+const AUTHED_ID = 'fakeID'
+
+export function handleInitialData() {
+    return (dispatch) => {
+        return getInitialData()
+            .then(({ users, questions }) => {
+                dispatch(receiveUsers(users))
+                dispatch(receiveQuestions(questions))
+                dispatch(setAuthedUser(AUTHED_ID))
+            })
+    }
+}
